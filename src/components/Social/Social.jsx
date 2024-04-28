9086e26973ce1fb5e4370a9226d12a0fc9e2ea2e
mohamad <78101279+mohamad1ghasemi@users.noreply.github.com>
2024-04-28 08:30:22 +0330

    added social menu

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -3,11 +3,58 @@ import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 function Social() {
-  return <div className="flex fixed flex-col top-[35%] left-0">
-    <ul>
-      {/* Start linkdin */}
-    </ul>
-  </div>;
+  return (
+    <div className="flex fixed flex-col top-[35%] left-0">
+      <ul>
+        {/* Start linkdin */}
+        <li className="w-[150px] h-[40px] pt-[5px] ml-[-100px] pr-2 pt-1 justify-between items-center hover:ml-[-10px] duration-300 bg-sky-500">
+          <a
+            href="/"
+            className="flex justify-between items-center w-full text-white"
+          >
+            LinkedIn
+            <FaLinkedin size={30} />
+          </a>
+        </li>
+        <li className="w-[150px] h-[40px] pt-[5px] ml-[-100px] pr-2 pt-1 justify-between items-center hover:ml-[-10px] duration-300 bg-[#333]">
+          <a
+            href="/"
+            className="flex justify-between items-center w-full text-white"
+          >
+            GitHub
+            <FaGithub size={30} />
+          </a>
+        </li>
+        <li className="w-[150px] h-[40px] pt-[5px] ml-[-100px] pr-2 pt-1 justify-between items-center hover:ml-[-10px] duration-300 bg-red-600">
+          <a
+            href="/"
+            className="flex justify-between items-center w-full text-white"
+          >
+            Instagram
+            <FaInstagram size={30} />
+          </a>
+        </li>
+        <li className="w-[150px] h-[40px] pt-[5px] ml-[-100px] pr-2 pt-1 justify-between items-center hover:ml-[-10px] duration-300 bg-green-700">
+          <a
+            href="/"
+            className="flex justify-between items-center w-full text-white"
+          >
+            Email
+            <HiOutlineMail size={30} />
+          </a>
+        </li>
+        <li className="w-[150px] h-[40px] pt-[5px] ml-[-100px] pr-2 pt-1 justify-between items-center hover:ml-[-10px] duration-300 bg-yellow-600">
+          <a
+            href="/"
+            className="flex justify-between items-center w-full text-white"
+          >
+            Resume
+            <BsFillPersonLinesFill size={30} />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Social;
